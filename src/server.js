0c48dac7297cdf3ea8391ff25a613a4e3fd0ad4d
Julia Martins <juliamartins.td2.senai@gmail.com>
2025-04-22 08:45:51 -0300

    feat: cors installation

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import avaliacaoRoutes from "./routes/avaliacaoRoutes.js"
+import cors from "cors";
 
 const app = express();
 const port = 4000;
+app.use(cors());
 app.use(express.json());
 app.use("/avaliacao", avaliacaoRoutes);
 app.listen(port, () => {
